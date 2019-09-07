7794fa107c918ec4505b5b185e6d88b423463b25
Wizviper777 <wizviper777@gmail.com>
2019-09-07 10:27:09 -0500

    Digging

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import './style.css';
 
 $(document).ready(function(){
+  var player = {
+    stone:0
+
+
+  }
   $("#tunneltab").hide();
 
   function changeScene(scene) {
@@ -14,12 +19,42 @@ $(document).ready(function(){
     }
   } 
 
+  function startDig() {
+    var digWidth = 1;
+    var digFrame = setInterval(digging, 10);
+    function digging() {
+    if(digWidth >= 100) {
+      clearInterval(digFrame);
+      $("#digBar").width(0 + "%");
+      player.stone++;
+    }
+    else {
+      digWidth++;
+      $("#digBar").css("width", digWidth + "%");
+    }
+  }
+  }
+
   $("#roombtn").click(function(){
   changeScene('room');
   });
   $("#tunnelbtn").click(function(){
   changeScene('tunnel');
   });
+  $("#digButton").click(startDig);
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 
