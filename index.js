2c435fcedbdd7a83dc6a03bd1c55f68fb6d65ed4
Wizviper777 <wizviper777@gmail.com>
2019-09-07 09:57:50 -0500

    Switching tabs

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 import './style.css';
 
+$(document).ready(function(){
+  $("#tunneltab").hide();
+
+  function changeScene(scene) {
+    if(scene == 'room') {
+    $("#roomtab").show();
+    $("#tunneltab").hide();
+    }
+    if(scene == 'tunnel') {
+    $("#roomtab").hide();
+    $("#tunneltab").show();
+    }
+  } 
+
+  $("#roombtn").click(function(){
+  changeScene('room');
+  });
+  $("#tunnelbtn").click(function(){
+  changeScene('tunnel');
+  });
+});
 
 
 
