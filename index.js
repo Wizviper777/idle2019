1088060c8ce038eb705fb03a34b80df99735085a
Wizviper777 <wizviper777@gmail.com>
2019-09-07 14:34:24 -0500

    Reinforcing Drill

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import './style.css';
 
 $(document).ready(function(){
   var player = {
-    stone:0
-
-
+    stone:10000,
+    drillPrice:10,
+    digTime:200,
+    digcheck:false
   }
   $("#tunneltab").hide();
 
@@ -20,19 +21,34 @@ $(document).ready(function(){
   } 
 
   function startDig() {
-    var digWidth = 1;
+    var digWidth = 0;
+    player.digcheck = true;
     var digFrame = setInterval(digging, 10);
     function digging() {
-    if(digWidth >= 100) {
+    if(digWidth >= player.digTime) {
       clearInterval(digFrame);
+      digWidth = 0;
       $("#digBar").width(0 + "%");
       player.stone++;
+      $("#stonetext").html("Stone: " + player.stone);
+      player.digcheck = false;
     }
     else {
       digWidth++;
-      $("#digBar").css("width", digWidth + "%");
+      $("#digBar").width((digWidth / player.digTime * 100) + "%");
     }
   }
+  }
+
+  function buyRoom(item) {
+    if(item == 'drill')
+    {
+      if(player.stone >= player.drillPrice) {
+        player.stone -= player.drillPrice;
+        player.drillPrice = Math.ceil(player.drillPrice * 1.3);
+        player.digTime *= 0.9;
+      }
+    }
   }
 
   $("#roombtn").click(function(){
@@ -41,7 +57,14 @@ $(document).ready(function(){
   $("#tunnelbtn").click(function(){
   changeScene('tunnel');
   });
-  $("#digButton").click(startDig);
+  $("#digButton").click(function() {
+    if(player.digcheck == false) {
+      startDig();
+    }
+  });
+    $("#drillButton").click(function(){
+  buyRoom('drill');
+  });
 
 
 
